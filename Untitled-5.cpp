fa25668b4bc754e65b89116cd8100c0ecd044bf6
Mantas24 <mantas.rimkus@stud.viko.lt>
2024-12-12 18:38:51 +0200

    Add files via upload

---
+++ Untitled-5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+
+{
+ // ivedu informacija 
+ string vardas = "Mantas";
+ string pavarde = "Rimkus";
+ int amzius = 15;
+ string grupe = "pi24/2";
+ int kursas = 1;
+ string studijuPrograma = "programų sistemos";
+
+// isvedu informacija
+
+ cout << "Studento informacija:" << endl;
+ cout << "Vardas: " << vardas << endl;
+ cout << "Pavardė: " << pavarde << endl;
+ cout << "Amžius: " << amzius << endl;
+ cout << "Grupė: " << grupe << endl;
+ cout << "Kursas: " << kursas << endl;
+ cout << "Studijų programa: " << studijuPrograma << endl;
+
+  return 0;
+}
